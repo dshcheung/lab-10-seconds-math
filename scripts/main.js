0c9c201fd9305a06921b391e5b577cdac50ba4c3
Denis Cheung <d.s.h.cheung@gmail.com>
2021-04-30 18:50:30 +0800

    bonus

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,15 @@ const $answerBox = $('#answer-box')
 const $secondsLeft = $('#secondsLeft')
 const $num1 = $('#num1')
 const $num2 = $('#num2')
+const $operator = $('#operator')
 const $solution = $('#solution-input')
 const $settings = $('#settings')
 const $maxNumber = $('#max-number')
 const $numberSetting = $('#number-setting')
+const $addSetting = $('#add-setting')
+const $subSetting = $('#sub-setting')
+const $multSetting = $('#mult-setting')
+const $powerSetting = $('#power-setting')
 
 // Gameover Box Elem
 const $gameOverBox = $('#game-over-box')
@@ -20,17 +25,27 @@ const REWARD_SECONDS = 10
 const REWARD_MS = REWARD_SECONDS * 1000
 const CLOCK_INVOKE_INTERVAL = 100
 
-// Others Global
+// Time
 let clockInterval, prevTime, timeLeft
+
+// Points & Question & Answer
 let points, answer, num1, num2
+
+// Settings
 let settings = {
   maxNum: 10,
-  add: true,
-  sub: false,
-  mult: false,
-  div: false,
-  power: false,
-  root: false
+  operators: {
+    add: true,
+    sub: false,
+    mult: false,
+    power: false
+  },
+  symbols: {
+    add: '+',
+    sub: '-',
+    mult: '*',
+    power: '**'
+  }
 }
 
 const gameOver = () => {
@@ -71,33 +86,53 @@ const startClock = () => {
   clockInterval = setInterval(updateSecondsLeft, CLOCK_INVOKE_INTERVAL)
 }
 
-const generateRandomInt = () => {
-  return Math.floor(Math.random() * settings.maxNum)
+const generateRandomInt = (max) => {
+  return Math.floor(Math.random() * max)
 }
 
 const generateRandomOperator = () => {
+  const availableOperators = Object.keys(settings.operators)
+  const enabledOperators = availableOperators.filter(key => settings.operators[key])
+  const randomOperator = enabledOperators[generateRandomInt(enabledOperators.length - 0.01)]
 
+  return settings.symbols[randomOperator]
 }
 
 const generateEquation = () => {
-  num1 = generateRandomInt()
-  num2 = generateRandomInt()
+  num1 = generateRandomInt(settings.maxNum)
+  num2 = generateRandomInt(settings.maxNum)
   operator = generateRandomOperator()
-  answer = num1 + num2
+
+  switch (operator) {
+    case '+':
+      answer = num1 + num2
+      break
+    case '-':
+      answer = num1 - num2
+      break
+    case '*':
+      answer = num1 * num2
+      break
+    case '^':
+      answer = num1 ** num2
+      break
+  }
 
   $num1.text(num1)
   $num2.text(num2)
+  $operator.text(operator)
 }
 
 const handleSettingsChange = () => {
   settings = {
+    ...settings,
     maxNum: Number($numberSetting.val()),
-    add: true,
-    sub: false,
-    mult: false,
-    div: false,
-    power: false,
-    root: false
+    operators: {
+      add: $addSetting.is(':checked'),
+      sub: $subSetting.is(':checked'),
+      mult: $multSetting.is(':checked'),
+      power: $powerSetting.is(':checked')
+    }
   }
 
   $maxNumber.text(settings.maxNum)
